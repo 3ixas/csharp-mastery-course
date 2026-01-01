393b821bd25f48c96087d6ea1a81a076d1219ac3
Elias Bennett <eliasthebennett@gmail.com>
2026-01-01 20:53:39 +0000

    Improved logic to check for parsing result first before range for Section 5: Conditional Statements 2 exercise

--- Course1-Fundamentals/Exercises/Section05-ControlFlow/ConditionalStatements2/Program.cs
+++ Course1-Fundamentals/Exercises/Section05-ControlFlow/ConditionalStatements2/Program.cs
@@ -6,19 +6,23 @@ class Program
     {
         // Write a program and ask the user to enter a number. The number should be between 1 to 10. If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid". (This logic is used a lot in applications where values entered into input boxes need to be validated.)
 
-        string userInput = Console.ReadLine();
-        Int32.TryParse(userInput, out int userInputInt);
-        // bool conversionSucceeded = Int32.TryParse(userInput, out int userInputInt);
+        Console.WriteLine("Please enter a number between 1 and 10");
 
-        if (userInputInt >= 1 && userInputInt <= 10)
+        string userInput = Console.ReadLine();
+        bool result = Int32.TryParse(userInput, out int userInputInt);
+
+        if (!result)
+        {
+            Console.WriteLine("Invalid - please enter a number between 1 and 10");
+        }
+        else if (userInputInt >= 1 && userInputInt <= 10)
         {
             Console.WriteLine("Valid");
         }
         else
         {
-            Console.WriteLine("Invalid");
+            Console.WriteLine("Invalid - number must be between 1 and 10");
         }
-
 
     }
 }
