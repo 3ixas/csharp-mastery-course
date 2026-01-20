f8d39777e2dfc1dc0c93839976c7981b47ded88a
Elias Bennett <eliasthebennett@gmail.com>
2026-01-20 17:43:55 +0000

    Complete Section 5: Conditional Statements 3 exercise

---
+++ Course1-Fundamentals/Exercises/Section05-ControlFlow/ConditionalStatements3/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConditionalStatements3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Write a program which takes two numbers from the console and displays the maximum of the two.
+
+        Console.WriteLine("Please enter a number: ");
+        string userInput1 = Console.ReadLine();
+        var resultA = Int32.TryParse(userInput1, out int userInputInt1);
+
+        Console.WriteLine("Please enter another number: ");
+        string userInput2 = Console.ReadLine();
+        var resultB = Int32.TryParse(userInput2, out int userInputInt2);
+
+        if (!resultA || !resultB)
+        {
+            Console.WriteLine("Invalid - please enter a number");
+        }
+        else if (userInputInt1 == userInputInt2)
+        {
+            Console.WriteLine($"{userInputInt1} is the same as {userInputInt2}, so both are the maximum number");
+        }
+        else if (userInputInt1 > userInputInt2)
+        {
+            Console.WriteLine($"{userInputInt1} is the maximum of the two numbers");
+        }
+        else
+        {
+            Console.WriteLine($"{userInputInt2} is the maximum of the two numbers");
+        }
+
+    }
+}
